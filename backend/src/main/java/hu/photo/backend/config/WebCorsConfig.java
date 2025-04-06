cceb88737a52f80350e87edaf842a3e099455d80
Kovács Domonkos <domonkoskovacs0@gmail.com>
2025-04-06 19:10:58 +0200

    add cors property

--- backend/src/main/java/hu/photo/backend/config/WebCorsConfig.java
+++ backend/src/main/java/hu/photo/backend/config/WebCorsConfig.java
@@ -1,12 +1,22 @@
 package hu.photo.backend.config;
 
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebCorsConfig {
+
+    private final CorsProperties corsProperties;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -14,11 +24,18 @@ public class WebCorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins(corsProperties.getPaths())
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(false);
             }
         };
+    }
+
+    @Data
+    @Component
+    @ConfigurationProperties("spring.security.cors.enabled")
+    public static class CorsProperties {
+        private String paths;
     }
 }
